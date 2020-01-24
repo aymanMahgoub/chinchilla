5780202015ba78c141d2d44f8e492c91b0f2f463
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2020-01-24 22:36:36 +0200

    [add] TestConnection

---
+++ tests/TestConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace chinchilla\tests;
+
+require __DIR__."/../config.php";
+
+use Chinchilla\Connection\Model\ConnectionOptions;
+use Chinchilla\Connection\Services\ConnectionFactory;
+use Chinchilla\Connection\Services\ConnectionInterface;
+use Chinchilla\Connection\Services\SocketConnection;
+use PhpAmqpLib\Connection\AbstractConnection;
+use PHPUnit\Framework\TestCase;
+use Chinchilla\Connection\Constant\ConnectionTypeConstant;
+
+/**
+ * Class TestConnection
+ *
+ * @package tests\Rabbitmq\Services
+ */
+class TestConnection extends TestCase
+{
+    public function testGetSocketConnection()
+    {
+        $connectionFactory = new ConnectionFactory();
+        $this->assertInstanceOf(
+            ConnectionInterface::class,
+            $connectionFactory::getConnectionService(ConnectionTypeConstant::SOCKET_CONNECTION_TYPE)
+        );
+    }
+
+    public function testSocketCreatConnection()
+    {
+        $socketConnection = new SocketConnection();
+        $connectionOption = new ConnectionOptions();
+        $this->assertInstanceOf(AbstractConnection::class, $socketConnection->createConnection($connectionOption));
+    }
+
+}
