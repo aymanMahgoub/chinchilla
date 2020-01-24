0b6063225fa9281a2af1f0476e2535c30a85c370
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2020-01-24 19:20:25 +0200

    [add] PhpAmqpLib connection type constant

---
+++ lib/Chinchilla/Connection/Constant/ConnectionTypeConstant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chinchilla\Connection\Constant;
+
+/**
+ * Class ConnectionTypeConstant
+ *
+ * @package Chinchilla\Connection\Constant
+ */
+class ConnectionTypeConstant
+{
+    public const SOCKET_CONNECTION_TYPE = 'socket';
+    public const SSL_CONNECTION_TYPE    = 'ssl';
+}
