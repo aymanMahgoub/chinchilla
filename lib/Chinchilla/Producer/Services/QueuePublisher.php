d33587cfb52cb055d7c0e44bd159f3dd879b0203
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2020-01-25 01:44:00 +0200

    [add] QueuePublisher with implementing ProducerInterface

---
+++ lib/Chinchilla/Producer/Services/QueuePublisher.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Chinchilla\Producer\Services;
+
+use Chinchilla\Connection\Model\ConnectionOptions;
+use Chinchilla\Connection\Services\ConnectionFactory;
+use Exception;
+use InvalidArgumentException;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AbstractConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * Class QueuePublisher
+ *
+ * @package Chinchilla\Producer\Services
+ */
+class QueuePublisher implements ProducerInterface
+{
+    /** @var array $queues */
+    private $queues;
+
+    /** @var array $producers */
+    private $producers;
+
+    /**
+     * QueuePublisher constructor.
+     */
+    public function __construct()
+    {
+        $queues          = file_get_contents(AMQP_QUEUES);
+        $this->queues    = json_decode($queues, true);
+        $this->producers = $this->queues["producers"];
+    }
+
+    /**
+     * @param string $body
+     * @param string $routingKey
+     *
+     * @throws Exception
+     */
+    public function publish(string $body, string $routingKey): void
+    {
+        if (!$this->isValidRoutingKey($routingKey)) {
+            throw new InvalidArgumentException('Passing undefined routingKey: '.$routingKey);
+        }
+        $amqpMessage    = $this->getAmqpMessage($body, $routingKey);
+        $producer       = $this->producers[$routingKey];
+        $amqpConnection = $this->getAmqpConnection($producer['connection']);
+        $channel        = $amqpConnection->channel();
+        $channel->basic_publish($amqpMessage, '', $routingKey);
+        $this->closeConnection($channel, $amqpConnection);
+    }
+
+
+    /**
+     * @param AMQPChannel        $channel
+     * @param AbstractConnection $abstractConnection
+     *
+     * @throws Exception
+     */
+    private function closeConnection(AMQPChannel $channel, AbstractConnection $abstractConnection)
+    {
+        $channel->close();
+        $abstractConnection->close();
+    }
+
+    /**
+     * @param string $connectionType
+     *
+     * @return AbstractConnection
+     */
+    private function getAmqpConnection(string $connectionType)
+    {
+        $rabbitConnection = ConnectionFactory::getConnectionService($connectionType);
+        $connectionOption = new ConnectionOptions();
+
+        return $rabbitConnection->createConnection($connectionOption);
+    }
+
+    /**
+     * @param string $body
+     * @param string $routingKey
+     *
+     * @return AMQPMessage
+     */
+    private function getAmqpMessage(string $body, string $routingKey)
+    {
+        $producers  = $this->queues["producers"];
+        $producer   = $producers[$routingKey];
+        $properties = $producer["properties"];
+
+        return new AMQPMessage($body, $properties);
+    }
+
+
+    /**
+     * @param string $routingKey
+     *
+     * @return bool
+     */
+    private function isValidRoutingKey(string $routingKey)
+    {
+        $producers = $this->queues['producers'];
+        if (array_key_exists($routingKey, $producers)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
