ef6688ef4cd8b6eb849b5773123f2a87bb43d215
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2020-01-27 23:07:12 +0200

    [add] consumerInterface and defind message flag constants

---
+++ lib/Chinchilla/Consumer/Services/ConsumerInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Chinchilla\Consumer\Services;
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * Class ConsumerInterface
+ *
+ * @package Chinchilla\Consumer\Services
+ */
+interface ConsumerInterface
+{
+    /**
+     * Flag for message ack
+     */
+    const MSG_ACK = 1;
+
+    /**
+     * Flag single for message nack and requeue
+     */
+    const MSG_SINGLE_NACK_REQUEUE = 2;
+
+    /**
+     * Flag for reject and requeue
+     */
+    const MSG_REJECT_REQUEUE = 0;
+
+    /**
+     * Flag for reject and drop
+     */
+    const MSG_REJECT = -1;
+
+    /**
+     * @param AMQPMessage $msg
+     *
+     * @return int
+     */
+    public static function consume(AMQPMessage $msg): int;
+
+}
