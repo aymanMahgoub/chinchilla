5f3fac13abe7a61cdf287bee2dbcf010cd8e75ce
Ayman Mahgoub <ayman.mahgoub1@gmail.com>
2020-01-31 16:10:55 +0200

    [add] fullDeleteQueue to delete queue from json file too

--- lib/Chinchilla/Consumer/Services/QueueDeletionService.php
+++ lib/Chinchilla/Consumer/Services/QueueDeletionService.php
@@ -4,6 +4,7 @@ namespace Chinchilla\Consumer\Services;
 
 use Chinchilla\Connection\Constant\ConnectionTypeConstant;
 use Chinchilla\Connection\Services\ChinchillaConnectionService;
+use InvalidArgumentException;
 
 /**
  * Class QueueDeletionService
@@ -13,15 +14,39 @@ use Chinchilla\Connection\Services\ChinchillaConnectionService;
 class QueueDeletionService
 {
     /**
-     * @param string $routingKey
+     * @param string $queueName
      */
-    public function partialDeleteQueue(string $routingKey): void
+    public function fullDeleteQueue(string $queueName): void
+    {
+        $queues = file_get_contents(AMQP_QUEUES);
+        $queues = json_decode($queues, true);
+        $consumers = $queues['consumers'];
+        $producers = $queues['producers'];
+        if (!array_key_exists($queueName, $queues['consumers'])
+            && !array_key_exists(
+                $queueName,
+                $queues['producers']
+            )) {
+            throw new InvalidArgumentException('There is no queue found with this name : '.$queueName);
+        }
+        $this->partialDeleteQueue($queueName);
+        unset($consumers[$queueName]);
+        unset($producers[$queueName]);
+        $queues['consumers'] = $consumers;
+        $queues['producers'] = $producers;
+        file_put_contents(AMQP_QUEUES, json_encode($queues));
+    }
+
+    /**
+     * @param string $queueName
+     */
+    public function partialDeleteQueue(string $queueName): void
     {
         $chinchillaConnection = ChinchillaConnectionService::openAmqpConnection(
             ConnectionTypeConstant::SOCKET_CONNECTION_TYPE
         );
         $channel              = $chinchillaConnection->channel();
-        $channel->queue_delete($routingKey);
+        $channel->queue_delete($queueName);
     }
 
 }
